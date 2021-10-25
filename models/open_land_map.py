3648580b5a514b7cc61bcda752da76e7a490121e
asathkumara <sathkumaraasel@gmail.com>
2021-10-25 11:53:11 -0700

    Added import for requests

--- models/open_land_map.py
+++ models/open_land_map.py
@@ -1,7 +1,8 @@
+import requests
+
 def some_api_call() -> list[str]:
     """
     Documenting makes your trees happier!
     :return: A list of happy trees
     """
     return ["Happy", "Trees", "2021"]
-
