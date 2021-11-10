ef051acba38ec3e237f2c8df5832509f63ba28af
Matthew Tkachuk <quicktkachuk@gmail.com>
2021-11-09 18:15:41 -0800

    Added OpenLandMap API call for getting layer information
    Gets the layers that are available for a given 'collection'.
    Returns a json file containing the information for the provided layer
    Returns a json containing all layers if 'collection' is empty or not a string

--- models/open_land_map.py
+++ models/open_land_map.py
@@ -1,11 +1,16 @@
 import requests
 
 
+def get_layers(collection):
+    """
+    Gets the layers that are available for a given 'collection'.
+    Returns a json file containing the information for the provided layer
+    Returns a json containing all layers if 'collection' is empty or not a string
+    """
+    if isinstance(collection, str) and collection:
+        response = requests.get('https://api.openlandmap.org/query/layers?coll={}'.format(collection))
 
-def get_collections() -> list[str]:
-    """
-    Gets the collections that are available from OpenLandMap.
-    :return: Returns a list of the available collections.
-    """
-    response = requests.get("https://api.openlandmap.org/query/collections")
+    else:
+        response = requests.get('https://api.openlandmap.org/query/layers?coll=all')
+
     return response.json()
