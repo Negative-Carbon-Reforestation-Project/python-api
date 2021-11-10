58192f96e1549f2f79ab71f204e5c44587f0e3ad
asathkumara <sathkumaraasel@gmail.com>
2021-11-09 18:25:33 -0800

    Re-added get_collections

--- models/open_land_map.py
+++ models/open_land_map.py
@@ -1,5 +1,12 @@
 import requests
 
+def get_collections() -> list[str]:
+    """
+    Gets the collections that are available from OpenLandMap.
+    :return: Returns a list of the available collections.
+    """
+    response = requests.get("https://api.openlandmap.org/query/collections")
+    return response.json()
 
 def get_layers(collection):
     """
