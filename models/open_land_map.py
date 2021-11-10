d66df00886765ad205d175998fec2022668f6dbc
Matt <59972585+quicktkachuk@users.noreply.github.com>
2021-11-09 18:34:07 -0800

    Update open_land_map.py

--- models/open_land_map.py
+++ models/open_land_map.py
@@ -11,8 +11,9 @@ def get_collections() -> list[str]:
 def get_layers(collection):
     """
     Gets the layers that are available for a given 'collection'.
-    Returns a json file containing the information for the provided layer
-    Returns a json containing all layers if 'collection' is empty or not a string
+    :param: A string that will be appended to the end of query url, used to specify the target collection
+    :returns: Returns a json file containing the information for the provided layer
+    :returns: Returns a json containing all layers if 'collection' is empty or not a string
     """
     if isinstance(collection, str) and collection:
         response = requests.get('https://api.openlandmap.org/query/layers?coll={}'.format(collection))
